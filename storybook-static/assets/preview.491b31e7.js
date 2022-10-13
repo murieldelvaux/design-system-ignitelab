06d1fbd75b40c049417ced665dad5fabdeb499c4
Muriel Delvaux Daher <muriel.delvaux@geb.inatel.br>
2022-10-13 12:09:03 -0300

    Remove: delete storybook-static folder

--- storybook-static/assets/preview.491b31e7.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.2b271aad.js";import"./iframe.4f190532.js";import"./es.map.constructor.39e9bfe0.js";import"./es.number.to-fixed.3c0ec98f.js";import"./index.cf57a8b2.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/},docs:{theme:o.dark}}};export{s as parameters};
-//# sourceMappingURL=preview.491b31e7.js.map
