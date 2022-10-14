70da913bb13fd4668290d2dbb2d1cfd72c1b56e1
Muriel Delvaux Daher <muriel.delvaux@geb.inatel.br>
2022-10-14 14:10:56 -0300

    test: add integration test with storybook and Jest

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,19 +7,24 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y"
+    '@storybook/addon-a11y'
   ],
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-vite"
   },
   "features": {
-    "storyStoreV7": true
+    "storyStoreV7": true,
+    "interactionsDebugger": true
   },
-  viteFinal: (config,{configType}) =>{
-    if(configType==='PRODUCTION'){
-      config.base = '/design-system-ignitelab/'
+  "staticDirs": [
+    "../public"
+  ],
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-lab-design-system/'
     }
-    return config;
+
+    return config
   }
 }
--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,5 +1,13 @@
 import { themes } from '@storybook/theming'
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
 import '../src/styles/global.css';
+
+initialize({
+  onUnhandledRequest: 'bypass' //para não mostrar o log das req que não tratamos com msw
+});
+
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,8 +16,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
-    docs:{
-      theme: themes.dark,
-    },
+  },
+  docs: {
+    theme: themes.dark,
   },
 }
